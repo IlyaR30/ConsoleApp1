38b03589fc5ec9d21c4710032888dee4a4161c69
Akademik <Akademik@DESKTOP-CJEEAB3>
2022-02-28 23:53:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+byte left = 1;
+byte right = 100;
+Console.WriteLine($"загадайте число от {left} до {right}");
+while (left < right) {
+    byte center = (byte)(left + (right - left) / 2);
+
+    
+    Console.WriteLine($"Ваше число больше {center} ?  введите \" yes\" or \"no \"):");
+    string? answer=Console.ReadLine();
+    if (answer == "yes") { left =(byte) (center+1); } else if(answer=="no"){
+        right = center; }
+    else {
+        continue;
+    };
+    }
+Console.WriteLine($"Ваше число {right}");
